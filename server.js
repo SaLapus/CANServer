916adbb9ee23d679cb3c2ef59dce4a42e70d92ca
SaLapus <onaera@bk.ru>
2021-12-13 22:14:34 +0300

    server update

--- server.js
+++ server.js
@@ -5,17 +5,24 @@ const { Devices } = require("./db");
 const app = express();
 const port = 3000;
 
+app.use(express.static("dist"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("dist/index.html");
 });
 
 app.get("/devices", async (req, res) => {
   const devices = await Devices.getALL(req.query);
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.json(devices);
 });
+
 app.get("/device", async (req, res) => {
   const devices = await Devices.getOne(req.query);
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.json(devices);
 });
